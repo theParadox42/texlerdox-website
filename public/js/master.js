ccf57da90eaf0edb29a556fd0a9878910e4ed478
Paradox Programming <nathaniel.fargo@gmail.com>
2019-04-25 18:24:00 -0600

    Bunch of fixes - Fixed center align - Navbar updates - Active coming soon - Added icon

--- public/js/master.js
+++ public/js/master.js
@@ -1,5 +1,5 @@
 function loadScript(url, callback) {
-    // Adding the script tag to the head as suggested before
+    // Adding the script tag to the body as suggested before
     var body = document.querySelector("body");
     var script = document.createElement('script');
     script.type = 'text/javascript';
@@ -13,11 +13,22 @@ function loadScript(url, callback) {
     // Fire the loading
     body.appendChild(script);
 };
+function loadIcon(url) {
+    // Adding the script tag to the head as suggested before
+    var head = document.querySelector("head");
+    var link = document.createElement("link");
+    link.rel = "shortcut icon";
+    link.href = url;
+
+    // Fire the loading
+    head.appendChild(link);
+
+}
 function doneLoading(){
     console.log("Done loading")
         $(document).ready(function(){
             var $nav = $('#navbar');
-            $nav.load("/assets/templates/navbar.html", function(){
+            $nav.load("/public/html/navbar.html", function(){
                 try {
                     if(active){
                         var $active = $('#' + active);
@@ -37,3 +48,4 @@ function loadPopper() {
     loadScript("/assets/libs/popper.min.js", loadBootstrap);
 };
 loadScript("/assets/libs/jquery-3.4.0.min.js", loadPopper);
+loadIcon("/assets/imgs/icon.png")
