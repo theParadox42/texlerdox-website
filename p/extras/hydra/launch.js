38e36b4e314dbf7c9a971a0fd7e8d222018e4cc1
Paradox Programming <nathaniel.fargo@gmail.com>
2019-04-26 13:30:26 -0600

    adg

--- p/extras/hydra/launch.js
+++ p/extras/hydra/launch.js
@@ -2,6 +2,15 @@ function launch(){
     var hydra = window.open("./hydra.html", '_blank', 'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000,top=10000,width=10,height=10,visible=none', '');
     if(!hydra) {
         alert("Allow popups and try again!")
+    } else {
+        return true;
     }
 }
-document.querySelector("button").addEventListener("click", launch)
+var clearNum = 0;
+document.querySelector("button").addEventListener("click", function(){
+    clearNum = setInterval(function(){
+        if(launch()){
+            clearInterval(clearNum)
+        }
+    }, 1000)
+})
